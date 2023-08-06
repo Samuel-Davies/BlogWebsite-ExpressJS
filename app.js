ef8ad97a93867626537533d5eda29ca233508404
SAMUEL DAVIES <yours@SAMUELs-MacBook-Pro.local>
2023-08-06 20:06:16 +0000

    implemented the create and read in CURD

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,16 +15,30 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// connecting to Db
+mongoose.connect('mongodb://localhost:27017/BlogDB');
 
-// global declarations 
+// creating a schma
+const BlogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required : [true, 'title cannot be ldeft black']
+  },
+  body:  {
+    type: String,
+    required: [ true, 'blogBody should not be blank']
+  }
+});
 
-const posts = [];
+// creating a model 
+const Post = mongoose.model('Post', BlogSchema);
 
 
 // home route
 
-app.get('/', (req, res)=>{
+app.get('/', async (req, res)=>{
 
+  const posts = await Post.find();
   res.render('home', {
     homeContent: homeStartingContent,
     posts: posts
@@ -49,35 +64,40 @@ app.get('/compose', (req, res)=>{
   res.render('compose');
 });
 
-app.post('/compose', (req, res)=>{
+app.post('/compose', async(req, res)=>{
 
-  const post = {
-    postBody : req.body.postBody, 
-  
-    postTitle: req.body.postTitle
-  };
+  await Post.create({
+    title: req.body.postTitle,
+    body: req.body.postBody 
+  }).then(
+    ()=>{
+      console.log("Successful Entry");
+    }
+  ).catch((err)=>{  
+    console.log(err);
+  });
 
-  posts.push(post);
 
   res.redirect('/');
   
-})
+});
 
 // dynamic urls 
-app.get('/posts/:postName', (req, res)=>{
-  const urlparamParsed = req.params.postName;
+app.get('/posts/:postId', async(req, res)=>{
+  const singlePostId = req.params.postId;
+  const posts = await Post.findOne({_id: singlePostId});
 
-  posts.forEach(post => {
-    if(_.lowerCase(urlparamParsed) === _.lowerCase(post.postTitle)){
-      res.render("post", {
-          title : post.postTitle,
-          content: post.postBody,
-          
-      });
-    }
+  res.render("post", {
+          title : posts.title,
+          content: posts.body,
+            
   });
 
-  
+  // posts.forEach(post => {
+  //   if(_.lowerCase(urlparamParsed) === _.lowerCase(post.title)){
+  //     
+  //   }
+  // });
 
  
 });
